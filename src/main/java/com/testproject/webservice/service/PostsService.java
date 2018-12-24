42f4921ee6f6cd5e57a055ce6fdbe4997285e0c1
Sungjin <yelloo98@enjoyworks.co.kr>
2018-12-24 11:12:14 +0900

    Handlebars 연동 / 게시글 등록

---
+++ src/main/java/com/testproject/webservice/service/PostsService.java
@@ -0,0 +1,19 @@
+package com.testproject.webservice.service;
+
+import com.testproject.webservice.domain.posts.PostsRepository;
+import com.testproject.webservice.dto.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@AllArgsConstructor
+@Service
+public class PostsService {
+    private PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto dto){
+        return postsRepository.save(dto.toEntity()).getId();
+    }
+}
--- src/main/java/com/testproject/webservice/web/WebRestController.java
+++ src/main/java/com/testproject/webservice/web/WebRestController.java
@@ -1,7 +1,8 @@
 package com.testproject.webservice.web;
 
-import com.testproject.webservice.domain.posts.PostsRepository;
+//import com.testproject.webservice.domain.posts.PostsRepository;
 import com.testproject.webservice.dto.posts.PostsSaveRequestDto;
+import com.testproject.webservice.service.PostsService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class WebRestController {
 
-    private PostsRepository postsRepository;
+//    private PostsRepository postsRepository;
+    private PostsService postsService;
 
     @GetMapping("/hello")
     public String hello() {
@@ -20,7 +22,10 @@ public class WebRestController {
     }
 
     @PostMapping("/posts")
-    public void savePosts(@RequestBody PostsSaveRequestDto dto){
-        postsRepository.save(dto.toEntity());
+    public Long savePosts(@RequestBody PostsSaveRequestDto dto){
+        return postsService.save(dto);
     }
+//    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+//        postsRepository.save(dto.toEntity());
+//    }
 }
